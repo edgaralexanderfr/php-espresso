83d491f9a8aa396b6fdf4288d63abcda245dfabb
Edgar Alexander Franco <edgaralexanderfr@gmail.com>
2024-05-18 23:11:56 -0500

    Create and define utils.h

---
+++ include/utils.h
@@ -0,0 +1,8 @@
+#ifndef UTILS_H
+#define UTILS_H
+
+#define count_uint16(array) (sizeof(array) / sizeof(uint16_t))
+#define count_string(array) (sizeof(array) / sizeof(string))
+#define count(array) (sizeof(array) / sizeof(string))
+
+#endif
