1f0e08378087207bc6b50fdf9e817840f541a5b3
Edgar Alexander Franco <edgaralexanderfr@gmail.com>
2024-05-26 21:29:35 -0500

    Fix strange '[Errno 48] Address already in use' error and correctly terminate the connection when sending "BYE".

--- src/http_linux.c
+++ src/http_linux.c
@@ -14,15 +14,31 @@ void espresso_http_server_listen(uint16 port)
 {
     extern int32 errno;
     struct sockaddr_in address;
+    socklen_t address_length = sizeof(address);
     int32 result;
 
     int32 server = socket(AF_INET, SOCK_STREAM, 0);
 
     if (server == -1)
     {
-        printf("%s\n", "Error while creating the socket.\n");
+        printf("%s\n", "Error while creating the socket.");
 
         exit(1);
+    }
+
+    int32 opt = 1;
+
+#ifdef __APPLE__
+    result = setsockopt(server, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
+#else
+    result = setsockopt(server, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt));
+#endif
+
+    if (result == -1)
+    {
+        printf("`setsockopt` error #%i.\n", errno);
+
+        exit(2);
     }
 
     address.sin_family = AF_INET;
@@ -35,25 +51,25 @@ void espresso_http_server_listen(uint16 port)
     {
         printf("Binding error #%i.\n", errno);
 
-        exit(2);
+        exit(3);
     }
 
     result = listen(server, 4);
 
     if (result == -1)
     {
-        printf("%s\n", "Error while listening the server.\n");
+        printf("%s\n", "Error while listening the server.");
 
-        exit(3);
+        exit(4);
     }
 
-    int32 client = accept(server, null, null);
+    int32 client = accept(server, (struct sockaddr *)&address, &address_length);
 
     if (client == -1)
     {
-        printf("%s\n", "Error while accepting the connection.\n");
+        printf("%s\n", "Error while accepting the connection.");
 
-        exit(4);
+        exit(5);
     }
 
     const byte buffer_size = 128;
@@ -69,7 +85,7 @@ void espresso_http_server_listen(uint16 port)
         printf("%s\n", buffer);
 
         send(client, response, response_length, 0);
-    } while (buffer[0] != 'B' && buffer[1] != 'Y' && buffer[2] != 'E');
+    } while (buffer[0] != 'B' || buffer[1] != 'Y' || buffer[2] != 'E');
 
     close(client);
     close(server);
